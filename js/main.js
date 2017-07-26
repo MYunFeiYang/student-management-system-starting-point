f0bae07a7a1daa23335e2b037dfec57c7c72d04d
MyunFeiYang <420222aa>
2017-07-26 10:18:07 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -83,6 +83,8 @@ function input_info_web_Storage() {
                 var chinese = document.getElementsByName('chinese')[i].value;
                 var english = document.getElementsByName('english')[i].value;
                 var program = document.getElementsByName('program')[i].value;
+                var sum_score=parseInt(math)+parseInt(chinese)+parseInt(english)+parseInt(program);
+                var ave_score=sum_score/4;
                 students.push({
                     'name': name,
                     'sno': sno,
@@ -91,7 +93,9 @@ function input_info_web_Storage() {
                     'math': math,
                     'chinese': chinese,
                     'english': english,
-                    'program': program
+                    'program': program,
+                    'sum_score':sum_score,
+                    'ave_score':ave_score
                 });
                 localStorage.students = JSON.stringify(students);
                 alert('添加成功');
@@ -105,8 +109,55 @@ function input_info_web_Storage() {
         alert("抱歉! 不支持 web 存储。");
     }
 }
+function show_students_info() {
+
+    if(typeof(Storage)!=="undefined")
+    {
+        var student_info=document.getElementById("show_students_info");
+        student_info.innerHTML="";
+        let students_result = JSON.parse(localStorage.students);
+        for (let i=0;i<students_result.length;i++) {
+            let tr = document.createElement('tr');
+            let td1 = document.createElement('td');
+            let td2 = document.createElement('td');
+            let td3 = document.createElement('td');
+            let td4 = document.createElement('td');
+            let td5 = document.createElement('td');
+            let td6 = document.createElement('td');
+            let td7 = document.createElement('td');
+            let td8 = document.createElement('td');
+            let td9 = document.createElement('td');
+            let td10 = document.createElement('td');
+            student_info.appendChild(tr);
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            tr.appendChild(td6);
+            tr.appendChild(td7);
+            tr.appendChild(td8);
+            tr.appendChild(td9);
+            tr.appendChild(td10);
+            td1.innerHTML = students_result[i].name;
+            td2.innerHTML = students_result[i].sno;
+            td3.innerHTML = students_result[i].native;
+            td4.innerHTML = students_result[i].Class;
+            td5.innerHTML = students_result[i].math;
+            td6.innerHTML = students_result[i].chinese;
+            td7.innerHTML = students_result[i].english;
+            td8.innerHTML = students_result[i].program;
+            td9.innerHTML = students_result[i].sum_score;
+            td10.innerHTML = students_result[i].ave_score;
+            debugger
+        }
+    } else {
+        alert("抱歉! 不支持 web 存储。");
+    }
+}
 function revise_info_web_Storage() {
-    
+    delete_info_web_Storage();
+    input_info_web_Storage();
 }
 function delete_info_web_Storage() {
     let sno=document.getElementsByName('sno');
@@ -145,6 +196,8 @@ function query_info_web_Storage() {
                     let td6 = document.createElement('td');
                     let td7 = document.createElement('td');
                     let td8 = document.createElement('td');
+                    let td9 = document.createElement('td');
+                    let td10 = document.createElement('td');
                     tbody.appendChild(tr);
                     tr.appendChild(td1);
                     tr.appendChild(td2);
@@ -154,6 +207,8 @@ function query_info_web_Storage() {
                     tr.appendChild(td6);
                     tr.appendChild(td7);
                     tr.appendChild(td8);
+                    tr.appendChild(td9);
+                    tr.appendChild(td10);
                     td1.innerHTML = students_result[i].name;
                     td2.innerHTML = students_result[i].sno;
                     td3.innerHTML = students_result[i].native;
@@ -162,6 +217,8 @@ function query_info_web_Storage() {
                     td6.innerHTML = students_result[i].chinese;
                     td7.innerHTML = students_result[i].english;
                     td8.innerHTML = students_result[i].program;
+                    td9.innerHTML = students_result[i].sum_score;
+                    td10.innerHTML = students_result[i].ave_score;
                 }
             }
         }
